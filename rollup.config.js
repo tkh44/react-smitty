f9baa2a6b5015be4dfa57b1cd0acc7600a656654
Kye Hohenberger <kye@sideway.com>
2017-04-25 09:58:28 -0600

    Copy rollup config from styled-components

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,14 @@ const pkg = JSON.parse(fs.readFileSync('./package.json'))
 export default {
   entry: 'src/index.js',
   external: ['react', 'smitty'],
-  globals: {react: 'react', smitty: 'smitty'},
+  globals: {react: 'React', smitty: 'smitty'},
+  moduleName: 'ReactSmitty',
   useStrict: false,
   sourceMap: true,
   plugins: [buble()],
   targets: [
     {dest: pkg.main, format: 'cjs'},
     {dest: pkg.module, format: 'es'},
-    {dest: pkg['umd:main'], format: 'umd', moduleName: pkg.amdName}
+    {dest: pkg['umd:main'], format: 'umd'}
   ]
 }
